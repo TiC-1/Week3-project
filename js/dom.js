bfc41ac4d87f00133d4f4c0c64ce5e065e2edba3
Giulia Spina <giulia.spina@gmail.com>
2018-02-21 15:20:17 +0100

    added stories to readme, index updated with instruction to user, dom manipulated

--- js/dom.js
+++ js/dom.js
@@ -1,10 +1,55 @@
-document.getElementById("searchForm").addEventListener("submit", function (event) {
+var container = document.getElementById('search-results');
+
+
+document.getElementById("searchForm").addEventListener("submit", function(event) {
   event.preventDefault();
-  getWeather( function () {
-    updateDOM(MUSICMETEO);
-  }, event.target.querySelector("input").value);
+  var location = event.target.querySelector("input").value;
+
+  console.log(location + " siamo qui");
+  // var getWeather = function() {
+  //     updateDOM(MUSICMETEO);
+  //   },
+
+  //da fare il link tra MUSICMETEO e location (statà nelle API)
+  console.log(MUSICMETEO);
+  updateDOM(MUSICMETEO);
+  // window.MUSICMETEO
+
 });
 
-function updateDOM (obj) {
-
+var createSongNode = function(song) {
+  //qui sotto creo ogni elemento li
+  console.log(song);
+  var songNode = document.createElement("li");
+  //songNode.className = "track";
+  //songNode.dataset.id = song.cover;
+  //var songButtonNode = document.createElement('button');
+  songNode.textContent = song.title;
+  //songNode.appendChild(songButtonNode);
+  //var songDescription = document.createTextNode("This is a song form the album" + state[i].album);
+  //songNode.appendChild(songDescription);
+  console.log(songNode);
+  return songNode;
 };
+
+
+var renderState = function(state) {
+  var songListNode = document.createElement('ul');
+  state.forEach(function(s) {
+    songListNode.appendChild(createSongNode(s));
+  });
+
+  // you may want to add a class for css
+  container.replaceChild(songListNode, container.firstChild);
+};
+
+function updateDOM() {
+  // 1. creare un nodo
+  // 2. inserirci le proprietà della canzone
+  var songs = MUSICMETEO.songs;
+  console.log(MUSICMETEO + " MUSICMETEO");
+  console.log(songs);
+  //var song=MUSICMETEO.songs[i];
+  if (container) renderState(songs);
+
+} //fine della funzione updateDOM
--- js/logic.js
+++ js/logic.js
@@ -1,33 +1,89 @@
 var MUSICMETEO = {
+  city: "Torino",
+  weatherName: "Sunny",
+  songs: [{
+      title: "SUNNY",
+      artist: "Girls' Generation",
+      album: "The 1st ASIA TOUR CONCERT: Into The New World",
+      cover: "http://s.mxmcdn.net/images-storage/albums/nocover.png",
+      genre: "Pop",
+    },
+
+    {
+      title: "Cinta Pertama",
+      artist: "Bunga C. Lestari",
+      album: "Cinta Pertama",
+      cover: "http://s.mxmcdn.net/images-storage/albums/nocover.png",
+      genre: "Pop",
+
+    },
+
+    {
+      title: "Sunny",
+      artist: "Super Junior",
+      album: "Super Show 2: The 2nd Asia Tour",
+      cover: "http://s.mxmcdn.net/images-storage/albums/nocover.png",
+      genre: "Pop",
+
+    },
+
+    {
+      title: "Sunny Sunny Cold Cold Day",
+      artist: "Herman Dune",
+      album: "Mas Cambios",
+      cover: "http://s.mxmcdn.net/images-storage/albums/nocover.png",
+      genre: "Pop",
+
+    },
+
+    {
+      title: "FOR BEAUTIFUL MAD HUMAN LIFE",
+      artist: "PENICILLIN",
+      album: "赫",
+      cover: "http://s.mxmcdn.net/images-storage/albums/nocover.png",
+      genre: "Pop",
+
+    },
+
+    {
+      title: "FOR BEAUTIFUL MAD HUMAN LIFE",
+      artist: "PENICILLIN",
+      album: "赫",
+      cover: "http://s.mxmcdn.net/images-storage/albums/nocover.png",
+      genre: "Pop",
+
+    },
+
+  ]
 
 };
 
 
-var getGif = function(textForm) {
-  var xhr = new XMLHttpRequest();
-  var url = "http://api.giphy.com/v1/gifs/search?q=" + textForm + "&api_key=dc6zaTOxFJmzC";
+// var getGif = function(textForm) {
+//   var xhr = new XMLHttpRequest();
+//   var url = "http://api.giphy.com/v1/gifs/search?q=" + textForm + "&api_key=dc6zaTOxFJmzC";
+//
+//   xhr.onreadystatechange = function() {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       var giphyObj = JSON.parse(xhr.responseText);
+//       var newImage;
+//       var gallery = document.getElementById("gallery");
+//       gallery.innerHTML = "";
+//       for (var i = 0; i < giphyObj.data.length; i++) {
+//         newImage = document.createElement("img");
+//         newImage.src = giphyObj.data[i].images.downsized_medium.url;
+//         gallery.appendChild(newImage);
+//       }
+//     }
+//   };
+//
+//   xhr.open("GET", url, true);
+//   xhr.send();
+// }
 
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var giphyObj = JSON.parse(xhr.responseText);
-      var newImage;
-      var gallery = document.getElementById("gallery");
-      gallery.innerHTML = "";
-      for (var i = 0; i < giphyObj.data.length; i++) {
-        newImage = document.createElement("img");
-        newImage.src = giphyObj.data[i].images.downsized_medium.url;
-        gallery.appendChild(newImage);
-      }
-    }
-  };
-
-  xhr.open("GET", url, true);
-  xhr.send();
-}
-
-document.getElementById("searchForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  var inputField = event.target.querySelector("input");
-  getGif(inputField.value);
-  inputField.value = "";
-});
+// document.getElementById("searchForm").addEventListener("submit", function(event) {
+//   event.preventDefault();
+//   var inputField = event.target.querySelector("input");
+//   getGif(inputField.value);
+//   inputField.value = "";
+// });
