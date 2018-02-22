ceb5d5c6e29d715d3dcc11362ef96865854cf7f3
Orionlight231 <orionlight231@gmail.com>
2018-02-22 17:35:29 +0100

    fixed test and logic #24 #26

---
+++ test/youtubeObj.js
@@ -0,0 +1,342 @@
+var youtubeObj = {
+  "kind": "youtube#searchListResponse",
+  "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/Zw-2c2aICkcwxh8E1SFDFyJdCYk\"",
+  "nextPageToken": "CAoQAA",
+  "regionCode": "IT",
+  "pageInfo": {
+    "totalResults": 1000000,
+    "resultsPerPage": 10
+  },
+  "items": [
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/ESeQg_tH9s_qF2vq0DpqPiu6gtg\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "8FOrfPpnhFI"
+      },
+      "snippet": {
+        "publishedAt": "2017-11-06T19:20:13.000Z",
+        "channelId": "UC2sZyDm1FqBWrWiCiAuI2Ug",
+        "title": "Undefined",
+        "description": "look an B E P i S Technically, the square root of -1 is i, an imaginary number. I was debating changing it, but that's how it is in the original. Original: https://redd.it/69mrq0 Discord:...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/8FOrfPpnhFI/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/8FOrfPpnhFI/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/8FOrfPpnhFI/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Timotainment",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/KiLPrN6rTAjenYOF8zHM_ZYX8zA\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "jbnddQ9l0IA"
+      },
+      "snippet": {
+        "publishedAt": "2015-02-16T02:59:55.000Z",
+        "channelId": "UCEgGmoOG70XNavCGGG6Ti0Q",
+        "title": "Undefined - Episode 1",
+        "description": "",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/jbnddQ9l0IA/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/jbnddQ9l0IA/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/jbnddQ9l0IA/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Nasty Na",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/bBmKG5b320LLO6stgPhP5_Y3VAI\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "alQ-_psHEcY"
+      },
+      "snippet": {
+        "publishedAt": "2013-01-01T14:21:15.000Z",
+        "channelId": "UCpiiqOzS7gQWbGTsJk_rbkg",
+        "title": "【初音ミク】Undefined【with中文字幕】",
+        "description": "曲名：Undefined -End of Sorrow- (未定之天 -悲慟的終結-) 作曲：てぃあら (http://www.nicovideo.jp/mylist/25325878) 來源：http://www.nicovideo.jp/watch/sm19702804 新年快樂!!...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/alQ-_psHEcY/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/alQ-_psHEcY/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/alQ-_psHEcY/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "swallow327",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/EWi1pzrge_peH0sTGBBlO6IZFDI\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "OL_tgFNnvKE"
+      },
+      "snippet": {
+        "publishedAt": "2017-09-15T16:15:50.000Z",
+        "channelId": "UCxzsnYV6KwqYm1aV1PHghDg",
+        "title": "UNDEFINED THE TRAILER",
+        "description": "I do not own the copy rights to this music or stock footage of Las Vegas (See copyright details below) Filmed and edited by Jordan McCormick MVP. Some shots by Dustin Robison of Quantum...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/OL_tgFNnvKE/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/OL_tgFNnvKE/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/OL_tgFNnvKE/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "UnDefined LesBshow",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/7S-5hs5ig6TYYqNkF2Kx75yLlD0\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "M8IRQxlvq2k"
+      },
+      "snippet": {
+        "publishedAt": "2015-12-26T11:40:52.000Z",
+        "channelId": "UCsNzXV2VSf8QhecD4fKbsOQ",
+        "title": "TDA凤凰旗袍ミク【初音ミク】Undefined【大神犬PV付】",
+        "description": "Miku is very beautiful~===credit in the vedio===Don't re-upload my videos on other websites===転載はご遠慮下さいますようお願い致します。 Model：https://bowlroll.net/file/88694.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/M8IRQxlvq2k/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/M8IRQxlvq2k/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/M8IRQxlvq2k/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "CME6 大神犬",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/hAXANjI5xpglYKoiyP3n4n4SXJU\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "6DYngiqZim0"
+      },
+      "snippet": {
+        "publishedAt": "2012-08-05T22:30:02.000Z",
+        "channelId": "UCGWIVzhbhwAqYC6osa_DY0w",
+        "title": "Hi-Rez - Undefined Ft. Cyn",
+        "description": "Download The Window: http://bit.ly/KkcVH6 http://www.facebook.com/HiRezFans http://twitter.com/HiRezTheRapper http://www.youtube.com/gm3beats Directed By JustCarlos (@JustCarlosNunez) Produced...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/6DYngiqZim0/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/6DYngiqZim0/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/6DYngiqZim0/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "HiRezTV",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/7MPZQhcPoPmy_YjoJ5v-8rDnLX4\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "TNjm0OuMSpo"
+      },
+      "snippet": {
+        "publishedAt": "2007-11-16T17:11:00.000Z",
+        "channelId": "UCwU4q4P05wyllt_Jmmm4V3Q",
+        "title": "Eternal UNDEFINED - The Music Video",
+        "description": "Eternal UNDEFINED - The Music Video Directed by Frederic Goossens www.undefinedsite.be.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/TNjm0OuMSpo/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/TNjm0OuMSpo/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/TNjm0OuMSpo/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "undefinedmusic",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/e5M43YHqc6mZ7mkI4vHXH-JjcRI\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "qMen9BIaFfs"
+      },
+      "snippet": {
+        "publishedAt": "2009-01-12T15:28:14.000Z",
+        "channelId": "UCwU4q4P05wyllt_Jmmm4V3Q",
+        "title": "Undefined - All The Galls - The Music Video",
+        "description": "Music video by Undefined performing \"All The Galls\" from the album \"Crimes Against Logic\". © 2012 Buy on itunes http://itunes.apple.com/be/album/all-the-galls-single/id303040947 Follow Undefined...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/qMen9BIaFfs/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/qMen9BIaFfs/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/qMen9BIaFfs/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "undefinedmusic",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/5y0jO-ajU8kbjVOgeu5_CHhLVIg\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "7MX60EW2t2o"
+      },
+      "snippet": {
+        "publishedAt": "2017-11-24T15:00:00.000Z",
+        "channelId": "UC8YarS7Dd3oQa5hyJZF0cYQ",
+        "title": "UNDEFINED | EPISODE ONE",
+        "description": "When a journalist discovers more than the Government wants, an agent is assigned to take him out. However, the agent is in a constant battle with himself deciding whats right from wrong. Follow...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/7MX60EW2t2o/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/7MX60EW2t2o/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/7MX60EW2t2o/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "TMS Productions",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/SzrBiNh3lGSXEawAMSLP3sjs8lg\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "G-9PTeUqGsk"
+      },
+      "snippet": {
+        "publishedAt": "2017-06-28T21:08:38.000Z",
+        "channelId": "UC47QA_Uw6uF1hSG_nOMFGqw",
+        "title": "U N D E F I N E D",
+        "description": "Meme Man Is Tristy And He Needs To Drink A Bepis But The Bepis Is Undefined And Meme Man Is Angery As An Undefined Now...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/G-9PTeUqGsk/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/G-9PTeUqGsk/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/G-9PTeUqGsk/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Zeyad We Ahmed Rezk",
+        "liveBroadcastContent": "none"
+      }
+    }
+  ]
+}
